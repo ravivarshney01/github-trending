cd23f7b4d837991ec1ba1f16ab06e6728590852f
Ravi Varshney <ravivarshney49@gmail.com>
2020-08-11 16:21:08 +0530

    project init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+import { render } from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Github trending</h1>
+    </div>
+  )
+}
+render(<App />, document.getElementById('root'))
